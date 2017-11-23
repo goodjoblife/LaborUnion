c3f5a2f20164bf4a2cc26e5e0a310e97068b448a
Wei-Ming <barry800414@gmail.com>
2017-11-24 01:05:36 +0800

    modify Rectangle

--- src/components/Rectangle.jsx
+++ src/components/Rectangle.jsx
@@ -3,15 +3,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledRectangle = styled.div`
-
+    border: 1px solid grey;
+    box-sizing: border-box;
+    width: 250px;
+    height: 250px;
+    padding-top: 30px;
+    .rec-icon {
+        width: 50px;
+        height: 50px;
+    }
 `;
 
-const Rectangle = ({ icon, title, content, onClick }) => {
+const Rectangle = ({ iconImg, title, content, onClick }) => {
     return (
-        <StyledRectangle onClick={onClick}>
-            <div className="icon"></div>
-            <div className="title"><span>{title}</span></div>
-            <div className="content"><p>{content}</p></div>
+        <StyledRectangle className="align-center" onClick={onClick}>
+            <img className="rec-icon" src={iconImg} />
+            <h3 className="rec-title align-center">{title}</h3>
+            <p className="rec-content align-center">{content}</p>
         </StyledRectangle>
     );
 }
