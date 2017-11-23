d8f8facef804d4d1504593c7f9efa73c979971e9
Wei-Ming <barry800414@gmail.com>
2017-11-23 21:35:09 +0800

    add banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import styled from 'styled-components';
+
+import bannerImg from '../assets/banner-img.png';
+
+const StyledBanner = styled.div`
+    width: 100%;
+    text-align: center;
+    padding: 80px 0;
+    .banner-img {
+        max-width: 100%;
+    }
+`;
+
+const Banner = () => (
+    <StyledBanner className="banner">
+        <img className="banner-img" src={bannerImg} />
+    </StyledBanner>
+);
+
+export default Banner;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Rectangle } from './Rectangle';
 export { default as Section } from './Section';
+export { default as Banner } from './Banner';
