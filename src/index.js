3ed5b7fb111a518a5115b00c903481a33212776d
Yen-chi Chen <mark86092@gmail.com>
2017-12-02 21:41:08 +0800

    disable service worker (#3)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 render(<App />, document.getElementById('root'));
-registerServiceWorker();
